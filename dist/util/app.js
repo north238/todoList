ea854631f410c816496e489823aef69d0d12a384
Fumiya <i.make.a.studious.effort@gmail.com>
2023-06-19 12:51:28 +0900

    bundle.jsファイルに変更

--- dist/util/app.js
+++
@@ -1,6 +0,0 @@
-import { TaskInput } from "../components/task-input";
-import { TaskList } from "../components/task-list";
-new TaskInput();
-new TaskList('active');
-new TaskList('finished');
-//# sourceMappingURL=app.js.map
